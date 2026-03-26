543578e73c0b3431496af2c3c9149eacfa296791
Wenjie Wang <cdend115@uni-trend.com.cn>
2026-03-26 09:19:15 +0800

    add install rulse.

--- build.sh
+++ build.sh
@@ -10,12 +10,16 @@ cmake	-DBUILD_LEPTON_DATA_COLLECTOR=ON \
 	-DBUILD_LEPTON_CONTROLLER=ON \
 	-DBUILD_SERVER=ON \
 	-DCMAKE_C_COMPILER=aarch64-linux-gnu-gcc \
-	-DCMAKE_CXX_COMPILER=aarch64-linux-gnu-g++  ..
+	-DCMAKE_CXX_COMPILER=aarch64-linux-gnu-g++ \
+	-DCMAKE_INSTALL_PREFIX=./output ..
 
 make
+make install
 
 cmake	-DBUILD_SDK=ON \
 	-DBUILD_LEPTON_SDK=ON \
-	-DBUILD_DEMO=ON ..
+	-DBUILD_DEMO=ON \
+	-DCMAKE_INSTALL_PREFIX=./sdk_output ..
 
 make
+make install
