640c9b35553271f0708e8d7e8e5a312857145eb5
Wenjie Wang <cdend115@uni-trend.com.cn>
2026-03-26 08:50:47 +0800

    modified:   CMakeLists.txt 	modified:   build.sh

--- build.sh
+++ build.sh
@@ -1,7 +1,21 @@
+rm build -r
+
 if [ ! -d "build" ]; then
 	mkdir build
 fi
 
 cd build
-cmake -DBUILD_ALL=ON -DCMAKE_C_COMPILER=arm-linux-gnueabihf-gcc -DCMAKE_CXX_COMPILER=arm-linux-gnueabihf-g++  ..
+
+cmake	-DBUILD_LEPTON_DATA_COLLECTOR=ON \
+	-DBUILD_LEPTON_CONTROLLER=ON \
+	-DBUILD_SERVER=ON \
+	-DCMAKE_C_COMPILER=aarch64-linux-gnu-gcc \
+	-DCMAKE_CXX_COMPILER=aarch64-linux-gnu-g++  ..
+
+make
+
+cmake	-DBUILD_SDK=ON \
+	-DBUILD_LEPTON_SDK=ON \
+	-DBUILD_DEMO=ON ..
+
 make
